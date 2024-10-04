7df907a9fd7663f376fd4dc66d36ecc673b3f88b
Ibrahim Raimi <raimiibrahim44@gmail.com>
2024-10-04 17:05:55 +0100

    remove unused assets

--- src/lib/data/index.ts
+++ src/lib/data/index.ts
@@ -1,44 +1,14 @@
-export const posts = [
-	{
-		id: 1,
-		title: "The Ultimate Guide to Agile Software Development.",
-		href: "https://dev.to/ibrahimraimi/the-ultimate-guide-to-agile-software-development-3cfi",
-		path: "https://metroboom.xyz/posts/agile-software-development"
-	},
-	{
-		id: 2,
-		title: "Taking a closer look at JavaScript package managers.",
-		href: "https://dev.to/ibrahimraimi/javascript-package-managers-npm-yarn-and-pnpm-5e9f",
-		path: "https://metroboom.xyz/posts/javascript-package-mamagers"
-	},
-	{
-		id: 3,
-		title: "Guide to data fetching in React with SWR.",
-		href: "https://dev.to/ibrahimraimi/data-fetching-in-react-with-swr-5gb0",
-		path: "https://metroboom.xyz/posts/data-fetching-with-swr"
-	},
-	{
-		id: 4,
-		title: "How to increase developer productivity",
-		href: "https://dev.to/ibrahimraimi/how-to-increase-developer-productivity-567o",
-		path: ""
-	}
-];
-
 export const projects = [
 	{
 		title: "Studio21",
-		path: "work/studio21",
 		href: "https://studio21.ibrahimraimi.com"
 	},
 	{
 		title: "Korty Eniola",
-		path: "/work/korty-eo",
 		href: "https://korty.ibrahimraimi.com"
 	},
 	{
 		title: "Color Palette Generator",
-		path: "work/palette",
 		href: "https://metro-palette.netlify.app"
 	}
 ];
