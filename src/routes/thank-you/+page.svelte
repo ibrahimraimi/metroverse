50d4d318360e1e2e8cc94ea5b866f3cf0399024d
Ibrahim Raimi <raimiibrahim44@gmail.com>
2024-11-05 11:13:06 +0100

    add thank you page

---
+++ src/routes/thank-you/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+</script>
+
+<div class="flex min-h-screen w-full items-center justify-center">
+	<div class="pace-y-8 text-center">
+		<div class="space-y-6">
+			<h1 class="text-4xl font-bold tracking-tight">Thank you!</h1>
+		</div>
+	</div>
+</div>
