d2fe001ef5ee72b59a72f0511654da3df4b858e0
Ibrahim Raimi <raimiibrahim44@gmail.com>
2024-11-05 11:16:48 +0100

    remove unused code

--- src/routes/thank-you/+page.svelte
+++ src/routes/thank-you/+page.svelte
@@ -1,6 +1,6 @@
 <div class="flex min-h-screen w-full items-center justify-center">
-	<div class="space-y-8 text-center">
-		<div class="space-y-6">
+	<div class="space-y-6 text-center">
+		<div class="space-y-4">
 			<h1 class="text-4xl font-bold tracking-tight">Thank you!</h1>
 		</div>
 	</div>
